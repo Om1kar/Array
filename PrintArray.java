3ad2e38d1eef8e0488ae715e8363cc9f5d9e101b
omkar <omkardudhewar121@gmail.com>
2022-11-25 14:59:12 +0530

    added:print array

---
+++ PrintArray.java
@@ -0,0 +1,10 @@
+package Array;
+
+public class PrintArray {
+    public static void main(String[] args) {
+        int[] array = new int[]{2, 4, 6, 8, 2};
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+}
