6810ebcd5a74f8705431658af7cdfce53c87cfc6
omkar <omkardudhewar121@gmail.com>
2022-11-26 14:24:31 +0530

    added:element at even position in a array

--- ArrayAtEvenPosition.java
+++ ArrayAtEvenPosition.java
@@ -1,2 +1,15 @@
-package Array;public class ArrayAtEvenPosition {
+package Array;
+ public class ArrayAtEvenPosition {
+     public static void main(String[] args) {
+         System.out.println("Elements of array at Even position =" );
+         int[] array = new int[]{1, 7, 5, 11, 9, 2};
+         /**
+          * index length-> 0 1 2 3 4 5
+          */
+         for (int i = 0; i < array.length; i++) {
+             if (i % 2 == 0) {
+                 System.out.print(array[i] + " ");
+             }
+         }
+     }
 }
