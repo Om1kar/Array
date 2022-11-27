8ef0d67b4b9af9c0e437c0c0fecd4a987d530742
omkar <omkardudhewar121@gmail.com>
2022-11-27 17:43:52 +0530

    added: to arrange arrray in ascending order

--- AscendingArray.java
+++ AscendingArray.java
@@ -1,2 +1,26 @@
-package Array;public class AscendingArray {
+package Array;
+
+public class AscendingArray {
+    public static void main(String[] args) {
+        System.out.println("Elements in Ascending Order = ");
+        int temp;  //variable to store the elements
+        int []array = new int[]{2, 12, 4, 5, 13, 9};
+        for (int i = 0; i < array.length; i++) {
+
+            for (int j = i + 1; j < array.length; j++) {
+                //comparing
+                if (array[i] > array[j]) {
+                    /*
+                    to swap the elements
+                     */
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp ;
+                }
+            }
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
 }
